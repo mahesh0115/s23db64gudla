0cf68f2ac322c0593285b69c8936de49c3a6eb44
mahesh0115 <133246966+mahesh0115@users.noreply.github.com>
2023-11-09 18:42:17 -0600

    Final commit

--- controllers/dumbells.js
+++ controllers/dumbells.js
@@ -44,3 +44,24 @@ exports.dumbell_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+
+    // Handle Costume create on POST.
+exports.dumbell_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Dumbell();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.dumbell_name = req.body.dumbell_name;
+    document.weight = req.body.weight;
+    document.material = req.body.material;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
