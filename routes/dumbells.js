93598890b7a5db6e077d18782a994ca6592e0c62
mahesh0115 <133246966+mahesh0115@users.noreply.github.com>
2023-11-29 22:01:44 -0600

    added secured func

--- routes/dumbells.js
+++ routes/dumbells.js
@@ -7,13 +7,20 @@ module.exports = router;
 
 // GET request for one costume.
 router.get('/dumbells/:id', dumbell_controlers.dumbell_detail);
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
 /* GET detail costume page */
 router.get('/detail', dumbell_controlers.dumbell_view_one_Page);
 /* GET create costume page */
 router.get('/create', dumbell_controlers.dumbell_create_Page);
 /* GET create update page */
-router.get('/update', dumbell_controlers.dumbell_update_Page);
+router.get('/update',secured, dumbell_controlers.dumbell_update_Page);
 /* GET delete costume page */
 router.get('/delete', dumbell_controlers.dumbell_delete_Page);
+
 
 
